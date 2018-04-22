aa8aabb161880f30e8cd00cd45226330708a0870
schmittjoaopedro <schmittjoaopedro@gmail.com>
2018-04-21 21:48:34 -0300

    Extended Euclid's algorithm

--- src/main/java/com/github/schmittjoaopedro/App.java
+++ src/main/java/com/github/schmittjoaopedro/App.java
@@ -9,6 +9,7 @@ import java.math.BigInteger;
  * [2] H. Java?, "How to generate a random BigInteger value in Java?", Stackoverflow.com, 2018. [Online]. Available: https://stackoverflow.com/questions/2290057/how-to-generate-a-random-biginteger-value-in-java. [Accessed: 20- Apr- 2018].
  * [3] w. David Ireland, "RSA Algorithm", Di-mgt.com.au, 2018. [Online]. Available: https://www.di-mgt.com.au/rsa_alg.html. [Accessed: 21- Apr- 2018].
  * [4] w. David Ireland, "The Euclidean Algorithm and the Extended Euclidean Algorithm", Di-mgt.com.au, 2018. [Online]. Available: https://www.di-mgt.com.au/euclidean.html. [Accessed: 21- Apr- 2018].
+ * [5] "Extended Euclidean Algorithm", Www-math.ucdenver.edu, 2018. [Online]. Available: http://www-math.ucdenver.edu/~wcherowi/courses/m5410/exeucalg.html. [Accessed: 22- Apr- 2018].
  */
 public class App  {
 
--- src/main/java/com/github/schmittjoaopedro/MDC.java
+++ src/main/java/com/github/schmittjoaopedro/MDC.java
@@ -43,69 +43,56 @@ public class MDC {
         return a;
     }
 
+    public BigInteger getModInv() {
+        if(a.compareTo(b) < 0) {
+            BigInteger aux = b;
+            b = a;
+            a = aux;
+        }
+        BigInteger a = this.a;
+        BigInteger b = this.b;
+        BigInteger p2 = new BigInteger("0");
+        BigInteger p1 = new BigInteger("1");
+        BigInteger q;
+        BigInteger p;
+        while (b.compareTo(new BigInteger("0")) > 0) {
+            BigInteger r = a.mod(b);
+            if(r.compareTo(new BigInteger("0")) > 0) {
+                q = a.divide(b);
+                p = p2.subtract(p1.multiply(q)).mod(this.a);
+                p2 = p1;
+                p1 = p;
+            }
+            a = b;
+            b = r;
+        }
+        return p1;
+    }
+
     public void calculateEuclidean() {
         if(a.compareTo(b) < 0) {
             BigInteger aux = a;
             a = b;
             b = aux;
         }
-        if(b.compareTo(new BigInteger("0")) == 0) {
-            d = a;
-            x = new BigInteger("1");
-            y = new BigInteger("0");
-        } else {
-            BigInteger a = this.a;
-            BigInteger b = this.b;
-            BigInteger x2 = new BigInteger("1");
-            BigInteger x1 = new BigInteger("0");
-            BigInteger y2 = new BigInteger("0");
-            BigInteger y1 = new BigInteger("1");
-            while(b.compareTo(new BigInteger("0")) > 0) {
-                // q = floor(a/b), r = a - q*b, x = x2 - q*x1, y = y2 - q*y1
-                BigInteger q = a.divide(b);
-                BigInteger r = a.subtract(q.multiply(b));
-                BigInteger x = x2.subtract(q.multiply(x1));
-                BigInteger y = y2.subtract(q.multiply(y1));
-                //a = b, b = r, x2 = x1, x1 = x, y2 = y1, y1 = y
-                a = b;
-                b = r;
-                x2 = x1;
-                x1 = x;
-                y2 = y1;
-                y1 = y;
-            }
-            d = a;
-            x = x2;
-            y = y2;
-        }
+        this.euclidExtension(this);
     }
 
-    public BigInteger getModInv() {
-        BigInteger inv, t1, t3, q;
-        BigInteger u1 = new BigInteger("1");
-        BigInteger u3 = a;
-        BigInteger v1 = new BigInteger("0");
-        BigInteger v3 = b;
-        BigInteger iter = new BigInteger("1");
-        while(v3.compareTo(new BigInteger("0")) != 0) {
-            q = u3.divide(v3);
-            t3 = u3.mod(v3);
-            t1 = u1.add(q.multiply(v1));
-            u1 = v1;
-            v1 = t1;
-            u3 = v3;
-            v3 = t3;
-            iter = iter.multiply(new BigInteger("-1"));
-        }
-        if(u3.compareTo(new BigInteger("1")) != 0) { //u3 != 1
-            throw new RuntimeException("Error: No inverse exists");
-        }
-        if(iter.compareTo(new BigInteger("0")) < 0) {
-            inv = b.subtract(u1);
+    private void euclidExtension(MDC mdc) {
+        if (mdc.b.compareTo(new BigInteger("0")) == 0) {
+            mdc.x = new BigInteger("1");
+            mdc.y = new BigInteger("0");
+            mdc.d = mdc.a;
         } else {
-            inv = u1;
+            MDC call = new MDC();
+            call.a = mdc.b;
+            call.b = mdc.a.mod(mdc.b);
+            euclidExtension(call);
+            mdc.x = new BigInteger(call.y.toString());
+            BigInteger abFloor = mdc.a.divide(mdc.b);
+            mdc.y = new BigInteger(call.x.subtract(abFloor.multiply(call.y)).toString());
+            mdc.d = call.d;
         }
-        return inv;
     }
 
 }

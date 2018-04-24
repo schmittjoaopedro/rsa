1707123c0326e48221b104079fc00a7ee3220444
schmittjoaopedro <schmittjoaopedro@gmail.com>
2018-04-23 22:22:38 -0300

    Trials

--- src/main/java/com/github/schmittjoaopedro/App.java
+++ src/main/java/com/github/schmittjoaopedro/App.java
@@ -1,5 +1,9 @@
 package com.github.schmittjoaopedro;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.FileWriter;
 import java.math.BigInteger;
 
 /**
@@ -17,14 +21,16 @@ public class App {
 
     public static void main(String[] args) {
         String msgPlain = "Curso de Pos Graduacao em Computacao Aplicada UDESC";
-        for(int i = 12; i < 4096; i++) {
-            testRSA(i, msgPlain);
+        for(int i = 18; i < 4096; i++) {
+            for(int t = 1; t < 30; t++) {
+                testRSA(i, t, msgPlain);
+            }
         }
     }
 
-    public static boolean testRSA(int bitSize, String msgPlain) {
+    public static boolean testRSA(int bitSize, int trial, String msgPlain) {
         RSA rsa = new RSA(bitSize);
-        String rec = bitSize + ";";
+        String rec = trial + ";" + bitSize + ";";
         try {
             long time = System.currentTimeMillis();
             rsa.generateKeys();
@@ -38,9 +44,11 @@ public class App {
             //System.out.println("Decrypted = " + msgDecrypt);
             if (msgPlain.equals(msgDecrypt)) {
                 String broken = BruteForce.broke(rsa, msgEncrypt);
-                rec += (System.currentTimeMillis() - time) + ";"; time = System.currentTimeMillis();
+                rec += (System.currentTimeMillis() - time);
                 if(broken.equals(msgDecrypt)) {
-                    System.out.println(rec);
+                    try {
+                        FileUtils.write(new File("result.csv"), rec, "UTF-8", true);
+                    } catch (Exception e) {}
                     return true;
                 }
             }
